aa9714ca8701ce690a8c88ebff19436f136bb892
StaraKoza <toni.stakovski@gmail.com>
2022-03-09 13:55:35 +0100

    Testing

---
+++ cypress/integration/2-advanced-examples/Yagitest/Yagitest5.spec.js
@@ -0,0 +1,12 @@
+describe('Infrastruktur', () => {
+    it('navigera till infrastruktur sektionen i sverige wiki', () => {
+      cy.visit('https://sv.wikipedia.org/wiki/Portal:Huvudsida')
+      cy.get('.vector-search-box-input')
+      .type('sverige').should('have.value', 'sverige')
+      .type('{enter}')
+      cy.get('[id=Infrastruktur]').click()
+
+
+      
+    })
+  })
